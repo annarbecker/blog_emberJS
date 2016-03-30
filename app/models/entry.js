c21316534407440170acea2930760841fe4089f8
Anna Becker and Michelle Brecunier <michelle.brecunier@gmail.com>
2016-03-30 09:14:24 -0700

    create entry model with sample objects connected to firebase

---
+++ app/models/entry.js
@@ -0,0 +1,10 @@
+import DS from 'ember-data';
+
+export default DS.Model.extend({
+  title: DS.attr(),
+  date: DS.attr(),
+  tagLine: DS.attr(),
+  body: DS.attr(),
+  category: DS.attr(),
+  author: DS.attr()
+});
